d5edc56059a7a9d925bd376d879c8ea1d9f720f8
Ulises Olivares-Pinto <uolivares@gdl.cinvestav.mx>
2021-07-17 01:44:52 -0500

    new changes

--- Figs/Rscripts/fig5/Fig 5 - transfeer.R
+++ Figs/Rscripts/fig5/Fig 5 - transfeer.R
@@ -4,23 +4,19 @@
 
 
 library(raster)
-library()
+library(tiff)
+
 
 # set Working dir
 setwd("~/Documents/GitHub/SouthInvassion/Figs/Rscripts/fig5")
 
-rastlist <- list.files(path = "~/Documents/GitHub/SouthInvassion/Figs/Rscripts/fig5", pattern='.tif$', all.files=TRUE, full.names=FALSE)
-rastFile <- mixedsort(rastlist)
+rastFile <- list.files(path = "~/Documents/GitHub/SouthInvassion/Figs/Rscripts/fig5", pattern='.tif$', all.files=TRUE, full.names=FALSE)
+#rastFile <- mixedsort(rastlist)
 
 # Stack all rasters
-mop <- stack(rastlist)
-
-mop <- mop[1]
+mop <- raster(rastFile)
 
 mop_df <- as.data.frame(mop)
 
 # Generate a plot from tiff
-ggplot() +
-  geom_raster(data = mop_df , aes(x = y, y = y, fill = HARV_dsmCrop)) +
-  scale_fill_viridis_c() +
-  coord_quickmap()
+plot(mop)
