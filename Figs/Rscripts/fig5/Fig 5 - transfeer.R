251e2d035e8f2877d57d12a6d506c6c2d8334380
Ulises Olivares-Pinto <uolivares@unam.mx>
2021-07-16 22:24:22 -0500

    generate a plot from raster

--- Figs/Rscripts/fig5/Fig 5 - transfeer.R
+++ Figs/Rscripts/fig5/Fig 5 - transfeer.R
@@ -13,6 +13,14 @@ rastlist <- list.files(path = "~/Documents/GitHub/SouthInvassion/Figs/Rscripts/f
 rastFile <- mixedsort(rastlist)
 
 # Stack all rasters
-bio <- stack(rastlist)
+mop <- stack(rastlist)
 
+mop <- mop[1]
 
+mop_df <- as.data.frame(mop)
+
+# Generate a plot from tiff
+ggplot() +
+  geom_raster(data = mop_df , aes(x = y, y = y, fill = HARV_dsmCrop)) +
+  scale_fill_viridis_c() +
+  coord_quickmap()
